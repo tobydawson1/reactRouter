69fbb18e186c59091dc5eec19d1fa552ff20449d
Toby Dawson <tobydawson1@gmail.com>
2020-06-17 14:09:12 +0100

    utilised switch to fix mulitple component routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Nav from './nav';
 import About from './about';
 import Shop from './shop';
-import {BrowserRouter as router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Router> 
       <div className="App">
         <Nav />
+        <Switch>
+        <Route path="/" exact component={Home} />
         <Route path="/about" component={About}/>
         <Route path="/shop" component={Shop}/>
+        </Switch>
       </div>
     </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1> Home Page</h1>
+  </div>
+);
 
 export default App;
  
