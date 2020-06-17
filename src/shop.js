283f83456cf487ec0f1e39ba0300311dce3acbb7
Toby Dawson <tobydawson1@gmail.com>
2020-06-17 14:51:56 +0100

    added api

--- src/shop.js
+++ src/shop.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function Shop() {
+
+  useEffect(() => {
+    fetchItems()
+  },[]);
+
+  const fetchItems = async () => {
+    const data = await fetch(`https://www.reddit.com/r/reactjs.json`
+    );
+
+    const items = await data.json();
+    console.log(items)
+  };
+
   return (
     <div className="App">
       <h1> Shop page </h1>
