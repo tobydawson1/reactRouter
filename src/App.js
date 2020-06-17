dd0b29d06a25d19f9f85ce329bb3c09f2f86efd5
Toby Dawson <tobydawson1@gmail.com>
2020-06-17 13:59:01 +0100

    added about and shop route

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import './App.css';
 import Nav from './nav';
 import About from './about';
 import Shop from './shop';
+import {BrowserRouter as router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <About />
-      <Shop />
-    </div>
+    <Router> 
+      <div className="App">
+        <Nav />
+        <Route path="/about" component={About}/>
+        <Route path="/shop" component={Shop}/>
+      </div>
+    </Router>
   );
 }
 
