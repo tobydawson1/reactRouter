17ccd24caa7a2c685b32447305225e250564aef0
Toby Dawson <tobydawson1@gmail.com>
2020-06-17 14:18:25 +0100

    added links into nav bar

--- src/nav.js
+++ src/nav.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Link} from 'react-router-dom';
 
 function Nav() {
+  const navStyle = {
+    color: 'white'
+  };
+
   return (
     <div className="App">
         <nav>
             <h3> Logo </h3>
             <ul className="nav-links">
+              <Link style={navStyle} to='/about'>
                 <li>About</li>
+              </Link>
+              <Link style={navStyle} to='/shop'>
                 <li>Shop</li>
+              </Link>
             </ul>
         </nav>
     </div>
